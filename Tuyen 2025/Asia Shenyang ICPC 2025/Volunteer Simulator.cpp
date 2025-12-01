8121b575deef2e9f1fa25746ecce381ece9b2736
D-V-A-T <doanvananhthai@gmail.com>
2025-12-01 14:47:56 +0700

    upd

---
+++ Tuyen 2025/Asia Shenyang ICPC 2025/Volunteer Simulator.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define bend(v) v.begin(),v.end()
+#define vect vector 
+#define prq priority_queue
+#define umap unordered_map
+#define eb emplace_back
+#define pb push_back
+#define pob pop_back
+#define ef emplace_front
+#define pf push_front
+#define pof pop_front
+#define el "\n"
+#define deb cout<<"\nok\n";return 
+#define nextl cout<<"\n"
+#define lwb lower_bound 
+#define upb upper_bound
+#define rs resize
+#define popcnt __builtin_popcount
+#define clz __builtin_clz
+#define ctz __builtin_ctz
+#define ll long long 
+#define dbl long double
+#define int long long
+
+
+#define FILE "ijustwannabepartofyourskibidi"
+void IO(){
+    if(fopen(FILE".in", "r")){
+        freopen(FILE".in", "r", stdin);
+        freopen(FILE".out", "w", stdout);
+    }
+    else if(fopen(FILE".inp", "r")){
+        freopen(FILE".inp", "r", stdin);
+        freopen(FILE".out", "w", stdout);
+    }
+}
+
+const ll N = 3e5 + 69, MOD = 1e9+7, INF = 1000000000000000069;
+
+mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+ll rand(ll l, ll r){
+    return uniform_int_distribution<ll>(l, r)(rng);
+}
+ll pm(ll a,const ll b=MOD){return ((a%b)+b)%b;}
+ll sq(ll x){return x*x;}
+ll __lcm(ll a, ll b, const ll lim=LLONG_MAX){
+    if(a == -1 || b == -1)return -1;
+    ll g = __gcd(a,b);
+    if(b/g > lim/a)return -1;
+    return (a/g)*b;
+}
+
+int firstAC[5000][5000];
+
+void sol(){
+    int n;
+    cin >> n;
+    vect<int> teams(N);
+
+    for(int i=0;n>i;i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        firstAC[a][b]++;
+
+        if(c >= 240){
+            if(firstAC[a][b]==1 && teams[a] < 3) teams[a]++, cout << b << el;
+            else cout << "0\n";
+        }else{     
+            if(firstAC[a][b] == 1) teams[a]++, cout << b << el;
+            else cout << "0\n";       
+        }
+
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);cout.tie(NULL);
+    IO();
+    int t = 1;
+    // cin >> t;
+    while(t--) sol();
+}
